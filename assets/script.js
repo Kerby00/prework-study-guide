0caf597930cf92ba4c225b767c4233228da56116
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2022-11-29 20:09:38 -0700

    added javascript topics and for loops

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,18 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topic.length; x++) {
+    console.log(topic[x]);
+}
+
+/*
+if (topic === 'HTML') {
+    console.log ("Let's study HTML");
+} else if (topic === 'CSS') {
+    console.log ("Let's study CSS!");
+} else if (topics === 'Git') {
+    console.log ("Let's study Git!");
+} else if (topics === 'JavaScript') {
+    console.log ("Let's study JavaScript!");
+} else {
+    console.log ('Please try agian!');
+} */
